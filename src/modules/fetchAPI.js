e9bea92c84def7e634cc507bfda1cf8547d7aa15
baroka <birotori@gmail.com>
2022-05-17 16:17:30 +0100

    Fix conflit on branche milestone 1

--- src/modules/fetchAPI.js
+++ src/modules/fetchAPI.js
@@ -204,49 +204,4 @@ const loadMealbyCategorie = async (mealData, categorieName, categorieDesc) => {
   });
 };
 
-
-const fetchMealbyCategorie = async (categorieName, categorieDesc) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorieName}`;
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  const mealData = jsonData.meals;
-  loadMealbyCategorie(mealData, categorieName, categorieDesc);
-  return mealData;
-};
-
-const addMeal = (mealData) => {
-  mealData.forEach((item) => {
-    const div = document.createElement('div');
-    div.classList.add('singleContainer');
-    div.innerHTML = `
-    <img src="${item.strCategoryThumb}" alt="${item.strCategory}">
-    <p class="categorieName">${item.strCategory}</p>
-    <p class="description">
-      ${item.strCategoryDescription}
-    </p>
-    <button class="showbtn" type="button" >show meals </button>
-    `;
-    main.appendChild(div);
-
-    const showbtn = document.querySelectorAll('button');
-    showbtn.forEach((item) => {
-      item.addEventListener('click', (e) => {
-        const categorieName = e.target.parentElement.querySelector('.categorieName');
-        const categorieDesc = e.target.parentElement.querySelector('.description');
-        fetchMealbyCategorie(categorieName.innerText, categorieDesc.innerText);
-      });
-    });
-  });
-};
-
-const fetchCategories = async () => {
-  const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  const mealCategoriesData = jsonData.categories;
-
-  addMeal(mealCategoriesData);
-  return mealCategoriesData;
-};
-
 export {fetchCategories, addCategorieMeal};
