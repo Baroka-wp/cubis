4c80ff15c1a7130f6555631ddeb6102f3e5789dc
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-17 18:31:52 +0100

    Modified the src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { fetchCategories, addCategorieMeal } from './modules/fetchAPI.js';
+const allCategorie = document.querySelector('.allCategorie');
 
 (async () => {
   const catCount = document.querySelector('.catCount');
@@ -8,3 +9,10 @@ import { fetchCategories, addCategorieMeal } from './modules/fetchAPI.js';
   catCount.innerHTML = `we have ${categorieList.length} Categories`;
   addCategorieMeal(categorieList);
 })();
+
+allCategorie.addEventListener('click', async () => {
+  const catCount = document.querySelector('.catCount');
+  const categorieList = await fetchCategories();
+  catCount.innerHTML = `we have ${categorieList.length} Categories`;
+  addCategorieMeal(categorieList);
+})
